466c919744dec420199f70485e1e899170958e43
Wrunko <45174850+Wrunko@users.noreply.github.com>
2019-05-27 15:55:15 +0200

    Update script_mqtt_mysql.py

--- script_mqtt_mysql.py
+++ script_mqtt_mysql.py
@@ -11,7 +11,7 @@ import mysql.connector as mysql
 
 ############ CODE MYSQL ############
 
-mydb = mysql.connect(host="localhost", user="root", passwd="", database="eflow")
+mydb = mysql.connect(host="localhost or other", user="your_username", passwd="your_psswd", database="your_namedatabase")
 mycursor = mydb.cursor()
 
 sql = "INSERT INTO donnees (data) VALUES (%s)"
@@ -24,7 +24,7 @@ def on_connect(client, userdata, flags, rc):
 
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
-    client.subscribe("/Kontron/TRACe/1117301020017/Lora/SX1276SEMTECH/e24f43fffe44c305/#")
+    client.subscribe("$SYS/#")
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
@@ -41,7 +41,7 @@ client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
 
-client.connect("kontron-lora.cloudapp.net", 1883, 60)
+client.connect("your_broker_address", "broker_port", 60)
 
 # Blocking call that processes network traffic, dispatches callbacks and
 # handles reconnecting.
